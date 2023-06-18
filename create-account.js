bc9f667e4b623532704d750e3dcb6e74494dd655
Daniel Tressel <dhtressel@gmail.com>
2023-06-17 23:10:10 -0400

    changed class to object

--- create-account.js
+++ create-account.js
@@ -1,17 +1,17 @@
 function createAccount(pin, balance = 0) {
-  class AccountActions {
-    static checkBalance(pinInput) {
+  return {
+    checkBalance: (pinInput) => {
       if (pinInput === pin) return `$${balance}`;
       return "Invalid PIN.";
-    }
-    static deposit(pinInput, depositAmount) {
+    },
+    deposit: (pinInput, depositAmount) => {
       if (pinInput === pin) {
         balance += depositAmount;
         return `Succesfully deposited $${depositAmount}. Current balance: $${balance}.`;
       }
       return "Invalid PIN.";
-    }
-    static withdraw(pinInput, withdrawalAmount) {
+    },
+    withdraw: (pinInput, withdrawalAmount) => {
       if (pinInput === pin) {
         if (withdrawalAmount > balance) {
           return "Withdrawal amount exceeds account balance. Transaction cancelled.";
@@ -20,8 +20,8 @@ function createAccount(pin, balance = 0) {
         return `Succesfully withdrew $${withdrawalAmount}. Current balance: $${balance}.`;
       }
       return "Invalid PIN.";
-    }
-    static changePin(oldPin, newPin) {
+    },
+    changePin: (oldPin, newPin) => {
       if (oldPin === pin) {
         pin = newPin;
         return "PIN successfully changed!";
@@ -29,7 +29,6 @@ function createAccount(pin, balance = 0) {
       return "Invalid PIN.";
     }
   }
-  return AccountActions;
 }
 
 module.exports = { createAccount };
