a347964dd4d88be1d30f2ccfc933daed3dccf550
Daniel Tressel <dhtressel@gmail.com>
2023-06-17 23:07:16 -0400

    finished 2 out of 3

--- create-account.js
+++ create-account.js
@@ -1,5 +1,35 @@
-function createAccount(pin, amount) {
-
+function createAccount(pin, balance = 0) {
+  class AccountActions {
+    static checkBalance(pinInput) {
+      if (pinInput === pin) return `$${balance}`;
+      return "Invalid PIN.";
+    }
+    static deposit(pinInput, depositAmount) {
+      if (pinInput === pin) {
+        balance += depositAmount;
+        return `Succesfully deposited $${depositAmount}. Current balance: $${balance}.`;
+      }
+      return "Invalid PIN.";
+    }
+    static withdraw(pinInput, withdrawalAmount) {
+      if (pinInput === pin) {
+        if (withdrawalAmount > balance) {
+          return "Withdrawal amount exceeds account balance. Transaction cancelled.";
+        }
+        balance -= withdrawalAmount;
+        return `Succesfully withdrew $${withdrawalAmount}. Current balance: $${balance}.`;
+      }
+      return "Invalid PIN.";
+    }
+    static changePin(oldPin, newPin) {
+      if (oldPin === pin) {
+        pin = newPin;
+        return "PIN successfully changed!";
+      }
+      return "Invalid PIN.";
+    }
+  }
+  return AccountActions;
 }
 
 module.exports = { createAccount };
--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,19 @@
 function guessingGame() {
-
+  const randomNum = Math.floor(Math.random() * 100);
+  let over = false;
+  let guessCount = 0;
+  function handleGuess(guess) {
+    if (over) return "The game is over, you already won!";
+    guessCount++;
+    if (guess < randomNum) return `${guess} is too low!`;
+    else if (guess > randomNum) return `${guess} is too high!`;
+    else {
+      over = true;
+      if (guessCount > 1) return `You win! You found ${guess} in ${guessCount} guesses.`;
+      else return `You win! You found ${guess} in 1 guess.`;
+    }
+  }
+  return handleGuess;
 }
 
 module.exports = { guessingGame };
